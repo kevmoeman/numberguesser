739f11f987791ef84e89f92e36ef01d0f979185f
Kevin Larsen <kevmoeman@gmail.com>
2016-12-18 03:08:28 -0800

    adds checkhandler

--- webserver.py
+++ webserver.py
@@ -2,17 +2,21 @@ import tornado.ioloop
 import tornado.web
 import json
 
-class MainHandler(tornado.web.RequestHandler):
+class CheckHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
+class MainHandler(tornado.web.RequestHandler):
     def post(self):
         data = tornado.escape.json_decode(self.request.body)
         print(data)
         self.write({"result":"0"})
+
+
 def make_app():
     return tornado.web.Application([
         (r"/numberguess", MainHandler),
+        (r"/", CheckHandler)
     ])
 
 if __name__ == "__main__":
